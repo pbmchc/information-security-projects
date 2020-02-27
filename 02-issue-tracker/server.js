686d0b7c3c55c104882d864b1d3aacbf7f8a0d81
PBM <pbmalecki@wp.pl>
2020-02-27 19:57:58 +0100

    Disabled browser caching, replaced powered by header

--- 02-issue-tracker/server.js
+++ 02-issue-tracker/server.js
@@ -2,19 +2,18 @@
 
 require('dotenv').config();
 
-var express     = require('express');
-var bodyParser  = require('body-parser');
-var expect      = require('chai').expect;
-var cors        = require('cors');
+const PORT = process.env.PORT || 3000;
+const express     = require('express');
+const bodyParser  = require('body-parser');
+const expect      = require('chai').expect;
+const cors        = require('cors');
 const helmet    = require('helmet');
 
-var apiRoutes         = require('./routes/api.js');
-var fccTestingRoutes  = require('./routes/fcctesting.js');
-var runner            = require('./test-runner');
+const apiRoutes         = require('./routes/api.js');
+const fccTestingRoutes  = require('./routes/fcctesting.js');
+const runner            = require('./test-runner');
 
-var app = express();
-
-const PORT = process.env.PORT || 3000;
+const app = express();
 
 app.use(helmet.xssFilter());
 
