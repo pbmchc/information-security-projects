9eaa4312cd5bc0bd67a7be48e4e55194047474b8
PBM <pbmalecki@wp.pl>
2020-03-02 22:38:08 +0100

    Implemented fetching stock data for comparison, switched to using promises for handling requests

--- 04-stock-price-checker/controllers/stockController.js
+++ 04-stock-price-checker/controllers/stockController.js
@@ -1,29 +1,51 @@
-const request = require('request');
+const request = require('request-promise');
 
 const {prepareErrorPayload} = require('../helpers/errorHelper');
 
 const STOCK_FETCHING_ERROR = 'Error while fetching stock';
+const STOCK_UNKNOWN_SYMBOL_ERROR = 'Unknown symbol';
 
-function getStock(req, res, next) {
+async function getStock(req, res, next) {
     const {query: {stock}} = req;
+    const tickers = Array.isArray(stock) ? stock : [stock];
 
-    request(_getStockRequestUrl(stock), (err, {body}) => {
-        if(err) {
-            return next(prepareErrorPayload(STOCK_FETCHING_ERROR));
-        }
+    try {
+        const records = await Promise.all(
+            tickers.map(ticker => request(_getStockRequestUrl(ticker)))
+        );
 
-        res.json(_getStockData(body));
-    });
+        res.json(_prepareStockData(records));
+    } catch(err) {
+        next(prepareErrorPayload(STOCK_FETCHING_ERROR));
+    }
 }
 
-function _getStockData(data) {
-    const {symbol: stock, latestPrice: price} = JSON.parse(data);
+function _prepareStockData(records) {
+    if(records.length > 1) {
+        return {
+            stockData: records.map(record => ({
+                ..._prepareBaseStockData(record),
+                rel_likes: 0
+            }))
+        };
+    }
 
+    return _prepareSingleStockRecord(records);
+}
+
+function _prepareSingleStockRecord([record]) {
     return {
-        stock,
-        price,
-        likes: 0
+        stockData: {
+            ..._prepareBaseStockData(record),
+            likes: 0
+        }
     };
+}
+
+function _prepareBaseStockData(record) {
+    const {symbol: stock, latestPrice: price} = JSON.parse(record);
+
+    return {stock, price};
 }
 
 function _getStockRequestUrl(stock) {
