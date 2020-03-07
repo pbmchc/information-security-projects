06547d7430bd0381bd49bd48ef14ee003937426a
PBM <pbmalecki@wp.pl>
2020-03-07 11:16:29 +0100

    Added functional tests for multiple stock tickers

--- 04-stock-price-checker/tests/2_functional-tests.js
+++ 04-stock-price-checker/tests/2_functional-tests.js
@@ -10,6 +10,7 @@ chai.use(chaiHttp);
 suite('Functional Tests', () => {
     suite('GET /api/stock-prices => stockData object', () => {
       const STOCK = 'goog';
+      const STOCKS = ['amzn', 'msft'];
       const clearTestCollection = (done) => Ticker.deleteMany({}).then(() => done());
 
       before((done) => {
@@ -24,7 +25,9 @@ suite('Functional Tests', () => {
        chai.request(server)
         .get('/api/stock-prices')
         .query({stock: STOCK})
-        .end((_, {body, status}) => {
+        .end((err, {body, status}) => {
+          if(err) return done(err);
+
           const {stockData: {stock, price, likes}} = body;
 
           assert.equal(status, 200);
@@ -40,7 +43,9 @@ suite('Functional Tests', () => {
         chai.request(server)
           .get('/api/stock-prices')
           .query({stock: STOCK, like: true})
-          .end((_, {body, status}) => {
+          .end((err, {body, status}) => {
+            if(err) return done(err);
+
             const {stockData: {stock, price, likes}} = body;
 
             assert.equal(status, 200);
@@ -56,7 +61,9 @@ suite('Functional Tests', () => {
         chai.request(server)
           .get('/api/stock-prices')
           .query({stock: STOCK, like: true})
-          .end((_, {body, status}) => {
+          .end((err, {body, status}) => {
+            if(err) return done(err);
+
             const {stockData: {stock, price, likes}} = body;
 
             assert.equal(status, 200);
@@ -68,12 +75,54 @@ suite('Functional Tests', () => {
           });
       });
       
-      test('2 stocks', function(done) {
-        
+      test('2 stocks', (done) => {
+        chai.request(server)
+          .get('/api/stock-prices')
+          .query({stock: STOCKS})
+          .end((err, {body, status}) => {
+            if(err) return done(err);
+
+            const {stockData} = body;
+            const [tickerA, tickerB] = stockData;
+
+            assert.equal(status, 200);
+            assert.isArray(stockData);
+            assert.equal(stockData.length, 2);
+            assert.equal(tickerA.stock, STOCKS[0].toUpperCase());
+            assert.isNumber(tickerA.price);
+            assert.isNumber(tickerA.rel_likes);
+            assert.equal(tickerA.rel_likes, 0);
+            assert.equal(tickerB.stock, STOCKS[1].toUpperCase());
+            assert.isNumber(tickerB.price);
+            assert.isNumber(tickerB.rel_likes);
+            assert.equal(tickerB.rel_likes, 0);
+            done();
+          });
       });
       
-      test('2 stocks with like', function(done) {
-        
+      test('2 stocks with like', (done) => {
+        chai.request(server)
+          .get('/api/stock-prices')
+          .query({stock: STOCKS, like: true})
+          .end((err, {body, status}) => {
+            if(err) return done(err);
+
+            const {stockData} = body;
+            const [tickerA, tickerB] = stockData;
+
+            assert.equal(status, 200);
+            assert.isArray(stockData);
+            assert.equal(stockData.length, 2);
+            assert.equal(tickerA.stock, STOCKS[0].toUpperCase());
+            assert.isNumber(tickerA.price);
+            assert.isNumber(tickerA.rel_likes);
+            assert.equal(tickerA.rel_likes, 0);
+            assert.equal(tickerB.stock, STOCKS[1].toUpperCase());
+            assert.isNumber(tickerB.price);
+            assert.isNumber(tickerB.rel_likes);
+            assert.equal(tickerB.rel_likes, 0);
+            done();
+          });
       });
     });
 });
