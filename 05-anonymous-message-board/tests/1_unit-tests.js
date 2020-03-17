1c3c754c0cd9e2546890e2b0001a5ab6b8e213f2
PBM <pbmalecki@wp.pl>
2020-03-17 22:39:27 +0100

    Added functional tests for creating threads

--- 05-anonymous-message-board/tests/1_unit-tests.js
+++ 05-anonymous-message-board/tests/1_unit-tests.js
@@ -1,6 +1,4 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-suite('Unit Tests', function(){
-
-});
+suite('Unit Tests', function(){});
--- 05-anonymous-message-board/tests/2_functional-tests.js
+++ 05-anonymous-message-board/tests/2_functional-tests.js
@@ -3,14 +3,39 @@ const chai = require('chai');
 const assert = chai.assert;
 const server = require('../server');
 
+const THREAD_TEST_PROJECT_BOARD = '__test__';
+const THREAD_REQUEST_URL = `/api/threads/${THREAD_TEST_PROJECT_BOARD}`;
+const THREAD_MOCK = {text: 'text', delete_password: 'password'};
+
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
-
   suite('API ROUTING FOR /api/threads/:board', function() {
-    
     suite('POST', function() {
-      
+      test('should return error message when required field is missing', (done) => {
+        const {delete_password} = THREAD_MOCK;
+        
+        chai.request(server)
+          .post(THREAD_REQUEST_URL)
+          .send({delete_password})
+          .end(({response: {status, text}}) => {
+            assert.equal(status, 400);
+            assert.equal(text, 'Missing required field: text');
+            done();
+          });
+      });
+
+      test('should redirect to board when thread has been successfully created', (done) => {        
+        chai.request(server)
+          .post(THREAD_REQUEST_URL)
+          .redirects(0)
+          .send(THREAD_MOCK)
+          .end((_, {status, text}) => {
+            assert.equal(status, 302);
+            assert.match(text, new RegExp(`/b/${THREAD_TEST_PROJECT_BOARD}`));
+            done();
+          });
+      });
     });
     
     suite('GET', function() {
@@ -22,14 +47,10 @@ suite('Functional Tests', function() {
     });
     
     suite('PUT', function() {
-      
     });
-    
-
   });
   
   suite('API ROUTING FOR /api/replies/:board', function() {
-    
     suite('POST', function() {
       
     });
@@ -47,5 +68,4 @@ suite('Functional Tests', function() {
     });
     
   });
-
 });
