4618637688802362b168a4805045eed0c5a00b92
PBM <pbmalecki@wp.pl>
2020-08-01 10:47:55 +0200

    Implemented updating puzzle input on sudoku board value change

--- 06-sudoku-solver/public/sudoku-solver.js
+++ 06-sudoku-solver/public/sudoku-solver.js
@@ -1,33 +1,68 @@
 import { puzzlesAndSolutions } from './puzzle-strings.js';
 
+const BOARD_INITIAL_CHARACTER_CODE = 65;
 const EMPTY_CELL_PLACEHOLDER = '.';
 const SAMPLE_PUZZLE = puzzlesAndSolutions[0][0];
 const VALID_PUZZLE_LENGTH = 81;
 
-const SUDOKU_CELL_CLASS = '.sudoku-input';
+const SUDOKU_BOARD_ID = 'sudoku-grid';
+const SUDOKU_CELL_CLASS = 'sudoku-input';
 const SUDOKU_INPUT_ID = 'text-input';
 
-const cells = document.querySelectorAll(SUDOKU_CELL_CLASS);
+const cells = document.querySelectorAll(`.${SUDOKU_CELL_CLASS}`);
 const puzzleInput = document.getElementById(SUDOKU_INPUT_ID);
+const sudokuBoard = document.getElementById(SUDOKU_BOARD_ID);
 
 document.addEventListener('DOMContentLoaded', () => {
   puzzleInput.value = SAMPLE_PUZZLE;
+
+  setBoardListener();
   setPuzzleInputListener();
-  updateSudokuBoard(puzzleInput.value);
+  updateBoard(puzzleInput.value);
 });
 
-function setPuzzleInputListener() {
-  puzzleInput.addEventListener('input', ({target: {value}}) => updateSudokuBoard(value));
+function setBoardListener() {
+  sudokuBoard.addEventListener('input', updatePuzzleInput);
 }
 
-function updateSudokuBoard(puzzle) {
-  const values = puzzle.split('');
+function setPuzzleInputListener() {
+  puzzleInput.addEventListener('input', ({target: {value}}) => updateBoard(value));
+}
 
-  if(values.length !== VALID_PUZZLE_LENGTH) {
+function updatePuzzleInput({target}) {
+  if(!target.classList.contains(SUDOKU_CELL_CLASS)) {
     return;
   }
 
-  values.forEach((value, index) => cells[index].value = value !== EMPTY_CELL_PLACEHOLDER ? value : '');
+  const puzzlePartIndex = getPuzzlePartIndex(target.id);
+  const puzzleValue = replacePuzzlePart(puzzleInput.value, target.value, puzzlePartIndex);
+
+  puzzleInput.value = puzzleValue;
+}
+
+function updateBoard(value) {
+  const puzzleParts = value.split('');
+
+  if(puzzleParts.length !== VALID_PUZZLE_LENGTH) {
+    return;
+  }
+
+  puzzleParts.forEach((part, index) => cells[index].value = part !== EMPTY_CELL_PLACEHOLDER ? part : '');
+}
+
+function getPuzzlePartIndex(position) {
+  const [_, column] = position.split('');
+  const rowMultiplier = getRowMultiplier(position.charCodeAt(0));
+
+  return rowMultiplier * 9 + Number(column) - 1;
+}
+
+function getRowMultiplier(code) {
+  return code - BOARD_INITIAL_CHARACTER_CODE;
+}
+
+function replacePuzzlePart(value, replacement, index) {
+  return `${value.substring(0, index)}${replacement || EMPTY_CELL_PLACEHOLDER}${value.substring(index + 1)}`;
 }
 
 try {
