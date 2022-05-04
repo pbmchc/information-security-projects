b16f866d126cf4577fce02d7f2a5cbc41ca2e135
pbmchc <pbmalecki@wp.pl>
2022-05-04 19:00:13 +0200

    feat: implement comparing passwords without salts

--- 09-password-cracker/password_cracker.py
+++ 09-password-cracker/password_cracker.py
@@ -1,5 +1,18 @@
 import hashlib
 
+KNOWN_SALTS_LIST = 'sources/known-salts.txt'
+TOP_PASSWORDS_LIST = 'sources/top-passwords.txt'
 
-def crack_sha1_hash(hash):
-    return None
+PASSWORD_NOT_FOUND = 'PASSWORD NOT IN DATABASE'
+
+
+def crack_sha1_hash(input_hash, use_salts=False):
+    with open(TOP_PASSWORDS_LIST) as top_passwords:
+        for line in top_passwords:
+            password = line.strip()
+            password_hash = hashlib.sha1(password.encode()).hexdigest()
+
+            if password_hash == input_hash:
+                return password
+
+    return PASSWORD_NOT_FOUND
