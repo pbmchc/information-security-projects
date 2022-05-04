ccf871aab5970c8572a40ec7540629d9ee2b3529
pbmchc <pbmalecki@wp.pl>
2022-05-04 19:56:00 +0200

    feat: implement comparing passwords with salts

--- 09-password-cracker/password_cracker.py
+++ 09-password-cracker/password_cracker.py
@@ -6,13 +6,40 @@ TOP_PASSWORDS_LIST = 'sources/top-passwords.txt'
 PASSWORD_NOT_FOUND = 'PASSWORD NOT IN DATABASE'
 
 
-def crack_sha1_hash(input_hash, use_salts=False):
+def crack_sha1_hash(checked_hash, use_salts=False):
+    salts = _get_salts() if use_salts else []
+
     with open(TOP_PASSWORDS_LIST) as top_passwords:
         for line in top_passwords:
             password = line.strip()
-            password_hash = hashlib.sha1(password.encode()).hexdigest()
 
-            if password_hash == input_hash:
+            if salts:
+                for salt in salts:
+                    if _is_matching_password_with_salt(password, salt, checked_hash):
+                        return password
+            elif _hash(password) == checked_hash:
                 return password
 
     return PASSWORD_NOT_FOUND
+
+
+def _get_salts():
+    salts = []
+
+    with open(KNOWN_SALTS_LIST) as known_salts:
+        for line in known_salts:
+            salt = line.strip()
+            salts.append(salt)
+
+    return salts
+
+
+def _is_matching_password_with_salt(password, salt, checked_hash):
+    password_with_salt_start = salt + password
+    password_with_salt_end = password + salt
+
+    return _hash(password_with_salt_start) == checked_hash or _hash(password_with_salt_end) == checked_hash
+
+
+def _hash(password):
+    return hashlib.sha1(password.encode()).hexdigest()
