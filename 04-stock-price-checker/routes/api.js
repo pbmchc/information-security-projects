b49a11924c449447abba4a04befb02a6008e167d
pbmchc <pbmalecki@wp.pl>
2022-08-08 19:28:18 +0200

    chore: bump mongodb and mongoose in stock price checker

--- 04-stock-price-checker/routes/api.js
+++ 04-stock-price-checker/routes/api.js
@@ -4,15 +4,7 @@ const mongoose = require('mongoose');
 
 const stockController = require('../controllers/stockController');
 
-mongoose.connect(
-  process.env.DB,
-  {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
-  }
-);
+mongoose.connect(process.env.DB);
 
 module.exports = function (app) {
   app.route('/api/stock-prices')
