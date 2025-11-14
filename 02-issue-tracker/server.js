709935fc820f10a7b627b60077c5b2cc60c386d5
pbmchc <pbmalecki@wp.pl>
2025-11-14 21:34:14 +0100

    feat: update helmet setup, use ejs in issue tracker

--- 02-issue-tracker/server.js
+++ 02-issue-tracker/server.js
@@ -5,27 +5,46 @@ import express from 'express';
 import helmet from 'helmet';
 
 import { HTTP_ERROR_CODES } from './constants/httpErrorCodes.js';
+import hat from './middlewares/hat.js';
 import { setupRoutes } from './routes/api.js';
 import { setupTestingRoutes } from './routes/fcctesting.js';
 import runner from './test-runner.js';
 
+const ENV = process.env.NODE_ENV || 'development';
+const IS_DEV = ENV === 'development';
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(helmet.xssFilter());
-
 app.use(cors({ origin: '*' })); // For FCC testing purposes
+
 app.use(express.json());
-app.use('/static', express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 
+app.use(hat.contentNonce());
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", 'code.jquery.com', (_req, res) => `'nonce-${res.locals.nonce}'`],
+      styleSrc: ["'self'"],
+      upgradeInsecureRequests: IS_DEV ? null : [],
+    },
+    useDefaults: false,
+  })
+);
+app.use(helmet.xPoweredBy());
+app.use(helmet.xXssProtection());
+
+app.use('/static', express.static('public'));
+app.set('view engine', 'ejs');
+
 app.route('/:project/').get(function (_, res) {
-  res.sendFile('views/issue.html', { root: import.meta.dirname });
+  res.render('issue');
 });
 
 app.route('/').get(function (_, res) {
-  res.sendFile('views/index.html', { root: import.meta.dirname });
+  res.render('index');
 });
 
 setupTestingRoutes(app); // For FCC testing purposes
