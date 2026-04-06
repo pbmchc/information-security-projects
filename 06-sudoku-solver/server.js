31725045b99e2dfcdc29619ecce734c82742f2cb
pbmchc <pbmalecki@wp.pl>
2026-04-06 16:16:19 +0200

    test: remove fcc tests leftovers from sudoku solver

--- 06-sudoku-solver/server.js
+++ 06-sudoku-solver/server.js
@@ -1,16 +1,11 @@
 import 'dotenv/config';
 
-import cors from 'cors';
 import express from 'express';
-
-import { setupTestingRoutes } from './routes/fcctesting.js';
-import runner from './test-runner.js';
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(cors({ origin: '*' })); // For FCC testing purposes only
 app.use(express.json());
 app.use('/static', express.static('public'));
 app.use(express.urlencoded({ extended: false }));
@@ -19,25 +14,12 @@ app.route('/').get(function (_, res) {
   res.sendFile('views/index.html', { root: import.meta.dirname });
 });
 
-setupTestingRoutes(app); // For FCC testing purposes
-
 app.use(function (_req, res, _next) {
   res.status(404).type('txt').send('Not Found');
 });
 
 app.listen(PORT, function () {
   console.log(`Listening on port: ${PORT}`);
-  if (process.env.NODE_ENV === 'test') {
-    console.log('Running Tests...');
-    setTimeout(function () {
-      try {
-        runner.run();
-      } catch (err) {
-        console.log('Error while running tests:');
-        console.log(err);
-      }
-    }, 1500);
-  }
 });
 
 export default app; // For FCC testing purposes
