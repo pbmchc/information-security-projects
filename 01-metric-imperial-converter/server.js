84e69843a50d6b243adeb46a6f78b80836352d6c
pbmchc <pbmalecki@wp.pl>
2026-04-06 15:14:14 +0200

    test: simplify metric imperial converter tests

--- 01-metric-imperial-converter/server.js
+++ 01-metric-imperial-converter/server.js
@@ -1,20 +1,15 @@
-import cors from 'cors';
 import express from 'express';
 import helmet from 'helmet';
 
 import { HTTP_ERROR_CODES } from './constants/httpErrorCodes.js';
 import hat from './middlewares/hat.js';
 import { setupRoutes } from './routes/api.js';
-import { setupTestingRoutes } from './routes/fcctesting.js';
-import runner from './test-runner.js';
 
 const ENV = process.env.NODE_ENV || 'development';
 const IS_DEV = ENV === 'development';
 const PORT = process.env.PORT || 3000;
 
 var app = express();
-
-app.use(cors({ origin: '*' })); // For FCC testing purposes
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -42,7 +37,6 @@ app.route('/').get(function (_, res) {
   res.render('index');
 });
 
-setupTestingRoutes(app); // For FCC testing purposes
 setupRoutes(app);
 
 app.use(function (_req, res, _next) {
@@ -51,17 +45,6 @@ app.use(function (_req, res, _next) {
 
 app.listen(PORT, function () {
   console.log(`Listening on port: ${PORT}`);
-  if (process.env.NODE_ENV === 'test') {
-    console.log('Running Tests...');
-    setTimeout(function () {
-      try {
-        runner.run();
-      } catch (err) {
-        console.log('Error while running tests:');
-        console.log(err);
-      }
-    }, 1500);
-  }
 });
 
 export default app; // For FCC testing purposes
