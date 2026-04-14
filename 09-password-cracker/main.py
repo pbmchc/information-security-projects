e57a9a92bb13b4fc5df6e45c62c01414bb2a55d5
pbmchc <pbmalecki@wp.pl>
2026-04-14 21:23:55 +0200

    debug: remove test comment from password cracker

--- 09-password-cracker/main.py
+++ 09-password-cracker/main.py
@@ -8,6 +8,5 @@ print(cracked_password1)
 cracked_password2 = password_cracker.crack_sha1_hash('dcc466796201f7232b22a03781110a8871fd038c', use_salts=True)
 print(cracked_password2)
 
-# Comment to trigger project test workflow
 # Run unit tests automatically
 main(module='test_module', exit=False)
